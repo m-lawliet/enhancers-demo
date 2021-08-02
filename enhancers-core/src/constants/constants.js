5dffeb61a73f343aea39c291c8081fca23717702
Marco Esciana <escianamarco@gmail.com>
2021-08-02 23:43:11 +0200

    Enhanced geolocation service [Issue #15]
    Took 14 minutes

--- enhancers-core/src/constants/constants.js
+++ enhancers-core/src/constants/constants.js
@@ -7,7 +7,13 @@ const DEFAULT_RESPONSES = Object.freeze({
   OK: 'Ok',
 });
 
+const DEFAULT_GEOLOCATION = Object.freeze({
+  LIMIT: 1,
+  LANGUAGE: 'en',
+});
+
 module.exports = {
   ERROR_CODES,
   DEFAULT_RESPONSES,
+  DEFAULT_GEOLOCATION,
 };
--- enhancers-core/src/geolocation/geolocationService.js
+++ enhancers-core/src/geolocation/geolocationService.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 
 const configSchema = require('./schemas/geolocationServiceConfig.json');
+const { DEFAULT_GEOLOCATION } = require('../constants/constants');
 
 class GeolocationService {
   constructor(config, shared) {
@@ -16,12 +17,42 @@ class GeolocationService {
     this.axios = axios.create({ baseURL, timeout });
   }
 
-  // TODO: Add log
-  async getLocationByName(name, options = {}) {
-    const { limit = 1 } = options;
+  async getLocationsByName(name, options = {}) {
+    let results = [];
     const { apiKey: appid, directGeocodingURL: url } = this;
+    const {
+      id,
+      limit = DEFAULT_GEOLOCATION.LIMIT,
+      lang = DEFAULT_GEOLOCATION.LANGUAGE,
+    } = options;
+    const logger = this.logger.child({ id, name, limit, lang });
     const params = { q: name, limit, appid };
-    return this.axios.get(url, { params });
+
+    try {
+      logger.debug('Retrieving locations...');
+      const { data } = await this.axios.get(url, { params });
+      results = data.map((result) => (
+        {
+          name: result.local_names[lang] || result.name,
+          country: result.country,
+          state: result.state,
+          lat: result.lat,
+          lon: result.lon,
+        }
+      ));
+      logger.debug('Retrieved locations');
+    } catch (error) {
+      logger.error('Cannot retrieve locations', { error });
+      throw error;
+    }
+    return results;
+  }
+
+  async getLocationByName(name, options = {}) {
+    const { id, lang } = options;
+    const limit = 1;
+    const [location] = await this.getLocationsByName(name,{ id, limit, lang });
+    return location;
   }
 }
 
